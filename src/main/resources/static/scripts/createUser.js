90178289cc57d163562d8b641a6b8637d8e823fb
Sebastian Nilsen <sebast@stud.ntnu.no>
2022-04-11 14:39:27 +0200

    Tried to add js for crate user but the post method doesn't work.

---
+++ src/main/resources/static/scripts/createUser.js
@@ -0,0 +1,25 @@
+
+
+
+$(document).ready(function(){
+
+    let formData = JSON.stringify($("#create-user-form").serializeArray());
+    
+    // click on button submit
+    $("#submit").on('click', function(){
+        // send ajax
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/customers",
+            data: formData,
+            dataType: "json",
+            contentType : "application/json",
+            success : function() {
+                console.log("success");
+            },
+            error: function() {
+                console.log("success");
+            }
+        })
+    });
+});
