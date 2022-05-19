b0d993e59eb3924a323f0ca569c74aaeafa38138
Fereshtaha <90913150+Fereshtaha@users.noreply.github.com>
2022-05-19 14:33:07 +0200

    Makiking changes to the footer and adding ManyToMany relation

--- src/main/java/no/ntnu/bicycle/model/Customer.java
+++ src/main/java/no/ntnu/bicycle/model/Customer.java
@@ -33,9 +33,17 @@ public class Customer {
     private BillingAndShippingAddress address;
     @Enumerated
     private Role role;
-    @OneToMany(targetEntity = Product.class, cascade = CascadeType.MERGE)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
+
+    @ManyToMany
+    @JoinTable(name="shoppingCart", joinColumns = @JoinColumn(name="customerId"), inverseJoinColumns = @JoinColumn(name="productId"))
+    //@OneToMany(targetEntity = Product.class, cascade = CascadeType.MERGE)
+    //@JoinColumn(name = "product_id", referencedColumnName = "id")
     private List<Product> shoppingCart;
+
+    //@ManyToMany
+    //@JoinTable(name="skriv nokka her", joinColumns = @JoinColumn(name="customerId"), inverseJoinColumns = @JoinColumn(name="productId"))
+    //private List<Product> products;
+
 
     //@OneToMany
     //@JsonIgnore
