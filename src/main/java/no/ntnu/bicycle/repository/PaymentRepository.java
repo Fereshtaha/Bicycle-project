ce0501cb6517b0b4f47b092109e7dc8ee9811539
Fereshtaha <90913150+Fereshtaha@users.noreply.github.com>
2022-06-02 15:22:37 +0200

    Trying to deploy website on server

--- src/main/java/no/ntnu/bicycle/repository/PaymentRepository.java
+++ src/main/java/no/ntnu/bicycle/repository/PaymentRepository.java
@@ -3,10 +3,14 @@ package no.ntnu.bicycle.repository;
 import no.ntnu.bicycle.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-   // @Query(value = "SELECT * FROM public.payment where customer_number = :")
+    @Query(value = "SELECT * FROM public.payment where customer_number = : customerNumber")
+    Optional<Payment> findByCustomerNumber(@Param("customerNumber") String customerNumber);
 
 }
